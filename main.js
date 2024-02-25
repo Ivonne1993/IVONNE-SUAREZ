dc96b726e9824f5cb3bbcfc4e0aa230468eed665
Ivonne1993 <161096996+Ivonne1993@users.noreply.github.com>
2024-02-25 16:51:48 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function muestra_oculta(id) {
+    let div = document.getElementById(id);
+    if (div.style.display == "none") {
+        div.style.display = "flex";
+    }
+    else{
+        div.style.display = "none";
+    }
+ }
+
+ function muestra_oculta(id) {
+    let div = document.getElementById(id);
+    if (div.style.display == "none") {
+        div.style.display = "flex";
+    }
+    else{
+        div.style.display = "none";
+    }
+ }
+
+ function muestra_oculta(id) {
+    let div = document.getElementById(id);
+    if (div.style.display == "none") {
+        div.style.display = "flex";
+    }
+    else{
+        div.style.display = "none";
+    }
+ }
